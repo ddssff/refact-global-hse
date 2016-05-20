cba8bdaa861c84d40f6cdd34a194b39f2c0bb495
David Fox <ddssff@gmail.com>
2016-05-20 11:04:45 -0700

    Add gitResetHard and gitIsClean to Utils

--- Utils.hs
+++ Utils.hs
@@ -2,6 +2,7 @@
 module Utils where
 
 import Control.Monad (MonadPlus, msum)
+import Data.Bool (bool)
 import Data.Generics (Data(gmapM), GenericM, listify, Typeable)
 import Data.Sequence (Seq, (|>))
 import System.Exit (ExitCode(..))
@@ -27,6 +28,15 @@ gFind = msum . map return . listify (const True)
 (|$>) :: Applicative m => m (Seq a) -> m a -> m (Seq a)
 (|$>) s x = (|>) <$> s <*> x
 
+-- | Do a hard reset of all the files of the repository containing the
+-- working directory.
+gitResetHard :: IO ()
+gitResetHard = do
+  (code, _out, _err) <- readProcessWithExitCode "git" ["reset", "--hard"] ""
+  case code of
+    ExitSuccess -> pure ()
+    ExitFailure _n -> error "gitResetHard"
+
 -- | Do a hard reset of all the files of a subdirectory within a git
 -- repository.  (Does this every throw an exception?)
 gitResetSubdir :: FilePath -> IO ()
@@ -35,3 +45,19 @@ gitResetSubdir dir = do
   case code of
     ExitSuccess -> pure ()
     ExitFailure _n -> error ("gitResetSubdir " ++ show dir)
+
+-- | Determine whether the repository containing the working directory
+-- is in a clean state.
+gitIsClean :: IO Bool
+gitIsClean = do
+  (code, out, _err) <- readProcessWithExitCode "git" ["status", "--porcelain"] ""
+  case code of
+    ExitFailure _ -> error "gitCheckClean failure"
+    ExitSuccess | all unmodified (lines out) -> pure True
+    ExitSuccess -> pure False
+    where
+      unmodified (a : b : _) = elem a "?! " && elem b "?! "
+      unmodified _ = False
+
+withCleanRepo :: IO a -> IO a
+withCleanRepo action = gitIsClean >>= bool (error "withCleanRepo: not clean") action
