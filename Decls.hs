da900b7d23b01af0c1d182c3b7ee3521dacc6eeb
David Fox <ddssff@gmail.com>
2016-05-22 13:37:27 -0700

    Fix combining function for MoveDecls

--- Decls.hs
+++ Decls.hs
@@ -32,10 +32,15 @@ type MoveSpec = ModuleKey -> A.Decl SrcSpanInfo -> ModuleKey
 appendMoveSpecs :: MoveSpec -> MoveSpec -> MoveSpec
 appendMoveSpecs f g =
     \k0 d ->
-        case (f k0 d, g k0 d) of
-          (k1, k2) | k1 == k2 -> k1
-          (k1, k2) | k1 == k0 -> k2
-          _ -> k0
+        let k1 = f k0 d
+            k2 = g k0 d in
+        if k1 == k0
+        then k2
+        else if k2 == k0
+             then k1
+             else if k1 == k2
+                  then k1
+                  else error "Conflicting move specs"
 
 identityMoveSpec = \k _ -> k
 
